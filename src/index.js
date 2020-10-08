123c37ddbc0b571dcabad88c78e6a7245278ffd4
Sanjib Roy <sanjibroy0098@gmail.com>
2020-10-08 13:13:11 +0530

    Basic setup of react-router-dom,node-sass,semantic ui and formik

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/stylesheets/index.scss";
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <BrowserRouter>
